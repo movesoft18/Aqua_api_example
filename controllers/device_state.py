0a003ef1064f76be350c9434ad5429d947735703
movesoft18 <movesoft@yandex.ru>
2025-09-26 11:30:24 +0400

    2

--- controllers/device_state.py
+++ controllers/device_state.py
@@ -26,4 +26,38 @@ class DeviceState(Resource):
             }, 200            
     
     def post(self):
-        pass
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument(
+            'deviceId',
+            type=int,
+            location='json',
+            required=True,
+            )
+        parser.add_argument(
+            'value',
+            type=int,
+            location='json',
+            required=True,
+            )        
+        args = parser.parse_args()
+        state = get_device_data(args['deviceId'])
+        if state is None:
+            return {
+                'error': 5,
+                'message': 'Объект, указанный в запросе, не найден',
+                'data': None,
+            }, 200
+        if state['type'] == 'sensor':
+            return {
+                'error': 7,
+                'message': 'Невозможно изменить состояние данного устройства',
+                'data': None,
+            }, 200            
+        if state['status'] != args['value']:
+            set_device_data(args['deviceId'], args['value'])
+            state['status'] = args['value']
+        return {
+            'error': 0,
+            'message': 'Ok',
+            'data': state
+        }, 200              
